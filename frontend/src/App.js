1735713d62f3e36636cf8a4f4ec91b5707b5ecd3
Xpliciton3 <blueline7639@gmail.com>
2026-04-08 09:44:14 -0500

    Refactor App.js to update routing and imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,41 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ErrorBoundary } from "./components/ErrorBoundary";
-import { ImperiumProvider } from "./lib/useImperiumApp";
-import Layout from "./components/Layout";
-import Dashboard from "./pages/Dashboard";
-import VelnarTutor from "./pages/VelnarTutor";
-import DailyPlanner from "./pages/DailyPlanner";
-import Translator from "./pages/Translator";
-import RiteOfUncrowned from "./pages/RiteOfUncrowned";
-import Meditations from "./pages/Meditations";
-import CalendarPage from "./pages/CalendarPage";
-import MealPlan from "./pages/MealPlan";
-import WarriorPractices from "./pages/WarriorPractices";
-import ShadowWork from "./pages/ShadowWork";
-import TrainingRegimen from "./pages/TrainingRegimen";
-import NutritionalArchitecture from "./pages/NutritionalArchitecture";
-import RitualPreparations from "./pages/RitualPreparations";
-import GlossaryPage from "./pages/GlossaryPage";
-import VelnarLanguageGuide from "./pages/VelnarLanguageGuide";
-import DoctrinePage from "./pages/DoctrinePage";
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ImperiumProvider } from './lib/useImperiumApp';
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+import DailyPlanner from './pages/DailyPlanner';
+import CalendarPage from './pages/CalendarPage';
+import MealPlan from './pages/MealPlan';
+import VelnarTutor from './pages/VelnarTutor';
+import WarriorPractices from './pages/WarriorPractices';
+import RiteOfUncrowned from './pages/RiteOfUncrowned';
+import GlossaryPage from './pages/GlossaryPage';
+import DoctrinePage from './pages/DoctrinePage';
 
 function App() {
   return (
-    <ErrorBoundary>
-      <ImperiumProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/tutor" element={<VelnarTutor />} />
-              <Route path="/planner" element={<DailyPlanner />} />
-              <Route path="/translator" element={<Translator />} />
-              <Route path="/rite" element={<RiteOfUncrowned />} />
-              <Route path="/meditations" element={<Meditations />} />
-              <Route path="/calendar" element={<CalendarPage />} />
+    <ImperiumProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/planner' element={<DailyPlanner />} />
+            <Route path='/calendar' element={<CalendarPage />} />
+            <Route path='/meals' element={<MealPlan />} />
+            <Route path='/tutor' element={<VelnarTutor />} />
+            <Route path='/warrior' element={<WarriorPractices />} />
+            <Route path='/rite' element={<RiteOfUncrowned />} />
+            <Route path='/glossary' element={<GlossaryPage />} />
+            <Route path='/doctrine' element={<DoctrinePage />} />
+            <Route path='*' element={<Navigate to='/' replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ImperiumProvider>
+  );
+}
+
+export default App;
               <Route path="/meals" element={<MealPlan />} />
               <Route path="/warrior" element={<WarriorPractices />} />
               <Route path="/shadow" element={<ShadowWork />} />
