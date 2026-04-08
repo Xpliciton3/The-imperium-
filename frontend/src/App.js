c20f98d1f7e61916debc23d80c692c62178a6cca
Xpliciton3 <blueline7639@gmail.com>
2026-04-08 12:32:26 -0500

    Refactor routes in App.js for cleaner structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,39 +17,21 @@ function App() {
     <ImperiumProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/planner' element={<DailyPlanner />} />
-            <Route path='/calendar' element={<CalendarPage />} />
-            <Route path='/meals' element={<MealPlan />} />
-            <Route path='/tutor' element={<VelnarTutor />} />
-            <Route path='/warrior' element={<WarriorPractices />} />
-            <Route path='/rite' element={<RiteOfUncrowned />} />
-            <Route path='/glossary' element={<GlossaryPage />} />
-            <Route path='/doctrine' element={<DoctrinePage />} />
-            <Route path='*' element={<Navigate to='/' replace />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="today" element={<DailyPlanner />} />
+            <Route path="calendar" element={<CalendarPage />} />
+            <Route path="meals" element={<MealPlan />} />
+            <Route path="velnar" element={<VelnarTutor />} />
+            <Route path="warrior" element={<WarriorPractices />} />
+            <Route path="rite" element={<RiteOfUncrowned />} />
+            <Route path="glossary" element={<GlossaryPage />} />
+            <Route path="doctrine" element={<DoctrinePage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </ImperiumProvider>
-  );
-}
-
-export default App;
-              <Route path="/meals" element={<MealPlan />} />
-              <Route path="/warrior" element={<WarriorPractices />} />
-              <Route path="/shadow" element={<ShadowWork />} />
-              <Route path="/training" element={<TrainingRegimen />} />
-              <Route path="/nutrition" element={<NutritionalArchitecture />} />
-              <Route path="/preparations" element={<RitualPreparations />} />
-              <Route path="/glossary" element={<GlossaryPage />} />
-              <Route path="/velnar-guide" element={<VelnarLanguageGuide />} />
-              <Route path="/doctrine" element={<DoctrinePage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ImperiumProvider>
-    </ErrorBoundary>
   );
 }
 
